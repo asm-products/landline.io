35f7fa4265987d479f9a54db0c9998dbd39bebf7
Charles Pletcher <cpletcher2@gmail.com>
2015-03-06 15:18:07 -0800

    Remove console.log

--- routes/signup.js
+++ routes/signup.js
@@ -11,7 +11,6 @@ module.exports = function(router) {
   });
 
   router.post('/signup', function(req, res) {
-    console.log(req.body);
     if (!req.body.email || !req.body.password || !req.body.name || !req.body.url) {
       return res.render('signup', {
         title: 'Landline | Signup',
